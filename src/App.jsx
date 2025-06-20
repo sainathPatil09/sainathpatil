fe743f84b4372d932f6bfa6d5b055144d3dd1b9d
sainathPatil09 <sainathpatilpanda@gmail.com>
2025-06-20 12:44:59 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
-import Navbar from './components/Navbar'
-import About from './components/About'
-import Projects from './components/Projects'
-import Contact from './components/Contact'
-import Home from './components/Home'
 import Index from './Pages'
 
 function App() {
